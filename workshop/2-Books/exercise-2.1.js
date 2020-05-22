93c8d6a6fa320111c0dcfa789d637587c459abd9
kolbymckeown <mckeownkolby@gmail.com>
2020-05-22 16:55:25 -0400

    Exercise 1-2 Complete

--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,33 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+	constructor(title, genre, author, isRead) {
+		this.title = title;
+		this.genre = genre;
+		this.author = author;
+		this.isRead = isRead || false;
+	}
+}
+
+let book1 = new Book("The Catcher in the Rye", "Novel", "J. D. Salinger");
+
+let book2 = new Book("Lord of the Rings", "Fantasy", "J. R. R. Tolkien");
+
+let book3 = new Book("Lord of the Flies", "Fiction", "William Golding");
+
+let book4 = new Book(
+	"The Hunger Games",
+	"Dystopian Fiction",
+	"Suzanne Collins",
+	true
+);
+
+let book5 = new Book(
+	"Band of Brothers",
+	"Non-fiction",
+	"Stephen E. Ambrose",
+	true
+);
 
 console.log(book1, book2, book3, book4, book5);
