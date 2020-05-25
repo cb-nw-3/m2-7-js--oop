90b09f185b81c64547862c74c656b4518bc7804b
bernard love constant <top_game29@hotmail.com>
2020-05-25 18:59:40 -0400

    m2-7-js--oop done

--- workshop/1-Cat/exercise-1.4.js
+++ workshop/1-Cat/exercise-1.4.js
@@ -11,3 +11,41 @@
 //    It's up to you to decide how quickly these values should change.
 
 // B) Make Boots wait 20 minutes and call then console.log(boots);
+class Cat {
+  constructor(name, breed) {
+    this.species = "cat";
+    this.breed = breed;
+    this.name = name;
+    this.tiredness = 30;
+    this.hunger = 30;
+    this.loneliness = 30;
+    this.happiness = 30;
+  }
+}
+
+sleep = (hours) => {
+  this.tiredness -= hours * 5;
+  this.happiness -= hours * 10;
+};
+
+eat = (food) => {
+  this.hunger -= (food * 1) / 5;
+  this.happiness -= (food * 1) / 15;
+};
+
+play = (minutes) => {
+  this.loneliness -= minutes * 3;
+  this.happiness -= minutes * 6;
+};
+
+wait = (minutes) => {
+  this.tiredness += minutes * 4;
+  this.hunger += minutes * 6;
+  this.loneliness += minutes * 2;
+  this.happiness -= minutes * 4;
+};
+const boots = new Cat("boots", "siamese");
+
+// boots.wait(1);
+
+console.log(boots);
--- workshop/2-Books/exercise-2.1.js
+++ workshop/2-Books/exercise-2.1.js
@@ -15,6 +15,23 @@
 //
 // Console.log them to verify that all is working.
 
-class Book {}
+class Book {
+  constructor(title, genre, author, isRead) {
+    this.title = title;
+    this.genre = genre;
+    this.author = author;
+    this.isRead = isRead || false;
+  }
+}
+
+const book1 = new Book("dark humour", "horror", "jean arnold");
+const book2 = new Book("shook ones", "comedy", "prodigy", true);
+const book3 = new Book("earth planet", "documentary", "kirl finlend");
+const book4 = new Book("death on elm street", "action", "roger peliaquin");
+const book5 = new Book(
+  "Harry Potter and the Chamber of Secrets",
+  "Fantasy",
+  "j.k. rowling"
+);
 
 console.log(book1, book2, book3, book4, book5);
